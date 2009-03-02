ef2e19a5fcca2b25db135747342eb4ad020eefc6
Randy Smith <git-general@tigana.org>
2009-03-01 20:23:10 -0500

    Resize and zoom mostly working
    There are edge case problems and the zoom doesn't keep quite the right
    location, but it's close.  Checkpointing.

--- image_widget.py
+++ image_widget.py
@@ -5,7 +5,40 @@ from Tkinter import *
 from PIL import Image
 import ImageTk
 
-debug = 0
+debug = 6
+
+## Interface decisions
+## 
+## Constraints I chose: 
+## 	* The canvas area is not allowed to extend past the currently
+## 	  displayed image.
+## 	* A relative zoom from the original image of 1.0 must always be
+## 	  acheivable through the scroll wheel
+##	* A zoom will never move the image (i.e. the point under the mouse
+##	  before the zoom will be under the mouse after).
+## Consequences:
+## 	* If a scroll wheel zoom would result in contracting the image
+## 	  to within the current boundaries of the canvas, it is
+## 	  ignored.
+## 	* If a resize would expand the canvas to an area outside the
+## 	  current image:
+## 		* If possible, the image is moved so that the canvas
+## 	  	  resize can occur, and
+## 		* The remainder of the implied resize is ignored.
+##	* "Ignoring resize" in the context of Tk is one of those almost
+##	  impossible things to do.  The interaction with the hierarchy
+##	  is that the slaves tell the master what they need, and the
+##	  master tries to give them that or more; less isn't really on
+##	  the agenda.  And that's reasonable; if you expand the top
+##	  level window, where should the space go?  And a comletely
+##	  separate notification structure would need to be in place
+##	  for the top level window to figure out that it shouldn't
+##	  expand.
+##
+##	  So that means that if you want to stop resize, you need to
+##	  do it at the very top level.  And that's doable
+##	  (toplevel.maxsize).  So what we need to do is link the
+##        maximum size of the widget to the top level configuration.
 
 ## Naming conventions for this file:
 ##	"dbg_" prefix: Debugigng function
@@ -61,7 +94,10 @@ class ImageWidget(Frame):
         TkInter PhotoImage object of size (xend-xstart, yend-ystart).
         IMAGE_SIZE describes the "base size" of the image being backed by
         gfunc.
-        starting_* describes the starting window on the image."""
+        starting_* describes the starting window on the image.
+        maxsize_callback is the function to be called when the
+        maximum size of the widget changes; it's called with
+        two args (width, height)."""
 
         ## Init parent
         Frame.__init__(self, parent)
@@ -77,7 +113,8 @@ class ImageWidget(Frame):
         ## (button press followed by button release without dragging)
         ## args are (x,y) and are in the unzoomed coordinate system
         self.click_func = kwargs.get("mouse_click_function", None)
-
+        self.maxsize_callback = kwargs.get("maxsize_callback", None)
+        
         ## XXX: See if there's anything you don't know about and if so throw an
         ## error
 
@@ -107,6 +144,7 @@ class ImageWidget(Frame):
         self.vscroll = Scrollbar(self, orient = VERTICAL,
                                  command = self.yview_action)
         
+        print "Setup: ", (self.canvas["width"], self.canvas["height"])
         (self.canvas["width"], self.canvas["height"]) = starting_size
 
         # "scrollregion" set in refresh method.
@@ -130,6 +168,58 @@ class ImageWidget(Frame):
         ## And display
         self.refresh()
 
+        ## Initial maxsize callback
+        self.maxsize_update()
+
+    def refresh(self):
+        """Bring the image in the frame and the scroll bars in line with the
+        current values."""
+
+        # Delete old image (if needed) 
+        if self.canvas_image_id:
+            self.canvas.delete(self.canvas_image_id)
+        if debug > 5:
+            print "refresh: New image (x", self.zoom, ") ", (self.xint, self.yint), (self.canvas["width"], self.canvas["height"]), [self.zoom * s for s in self.isize]
+
+        scaled_isize = [self.xint[1] - self.xint[0],
+                        self.yint[1] - self.yint[0]]
+
+        # Create the image for the canvas
+        self.image = self.generator_func(self.zoom, self.xint, self.yint)
+        self.canvas_image_id = self.canvas.create_image(0, 0, anchor=N+W,
+                                                        image=self.image)
+
+        # Figure out where scroll bars should be and put them there.
+        if self.xint[0] == 0 and int(self.isize[0] * self.zoom) == self.xint[1]:
+            self.hscroll.grid_remove()
+        else:
+            self.hscroll.grid()
+            self.hscroll.set(mapped_number(self.xint[0],
+                                           (0, self.isize[0] * self.zoom -1),
+                                           (0, 1)),
+                             mapped_number(self.xint[1] -1,
+                                           (0, self.isize[0] * self.zoom -1),
+                                           (0, 1)))
+        if self.yint[0] == 0 and int(self.isize[1] * self.zoom) == self.yint[1]:
+            self.vscroll.grid_remove()
+        else:
+            self.vscroll.grid()
+            self.vscroll.set(mapped_number(self.yint[0],
+                                           (0, self.isize[1] * self.zoom -1),
+                                           (0, 1)),
+                             mapped_number(self.yint[1] -1,
+                                           (0, self.isize[1] * self.zoom -1),
+                                           (0, 1)))
+
+    def maxsize_update(self):
+        self.update_idletasks()
+        if self.maxsize_callback:
+            diff = (self.winfo_width() - self.canvas.winfo_width(),
+                    self.winfo_height() - self.canvas.winfo_height())
+            self.maxsize_callback(int(self.zoom * self.isize[0]) + diff[0],
+                                  int(self.zoom * self.isize[1]) + diff[1])
+        
+
     ## XXX: Need to figure out and specify what coordinates arguments to the
     ## action functions should be in.
     ## Currently, canvas coords
@@ -141,23 +231,74 @@ class ImageWidget(Frame):
     def click_action(self, coord):
         "Respond as appropriate to the user clicking the mouse button in coord x,y."
         if self.click_func:
+            # XXX: Canvas coords need to be mapped more explicitly.
             self.click_func(int(coord[0] / self.zoom), int(coord[1] / self.zoom))
 
-    def scrollWheel_action(self, count):
+    def scrollWheel_action(self, count, location):
         "Respond as appropriate to the scroll wheel being clicked count times."
         ## Turn into hardcoded constant
         zoomFactor = pow(1.20, count)
-        self.zoom = zoomFactor
+        view_size = (self.xint[1] - self.xint[0], self.yint[1] - self.yint[0])
+        # Block if it's going to be smaller than we can display
+        print self.isize, self.zoom, view_size
+        if (self.isize[0] * self.zoom < view_size[0]
+            or self.isize[1] * self.zoom < view_size[1]):
+            return
+
+        # Compute (scaled) image coords of current point
+        cloc = (int(self.canvas.canvasx(location[0])),
+                int(self.canvas.canvasy(location[1])))
+        loc = (cloc[0] + self.xint[0], cloc[1] + self.yint[0])
+
+        # Modify by zoom
+        loc = (int(loc[0] * zoomFactor), int(loc[1] * zoomFactor))
+
+        # Compute new xint and yint on this basis
+        xi = [loc[0] - cloc[0], loc[0] - cloc[0] + view_size[0]]
+        yi = [loc[1] - cloc[1], loc[1] - cloc[1] + view_size[1]]
+
+
+        print xi, yi, self.zoom, zoomFactor, self.isize
+        if (xi[0] < 0 or xi[1] > self.zoom * zoomFactor * self.isize[0]
+            or yi[0] < 0 or yi[1] > self.zoom * zoomFactor * self.isize[0]):
+            # Ignore event
+            return
+
+        self.zoom *= zoomFactor
+        self.xint = xi
+        self.yint = yi
         self.refresh()
+        self.maxsize_update()
+
+    @staticmethod
+    def resize_view_axis(interval, newsize, image_length):
+        """Do the work of expanding the view window along a single axis."""
+        if newsize < image_length - interval[0]:
+            # Window can be expanded without any shift of image or whitespace
+            interval[1] = interval[0] + newsize
+        elif newsize < image_length:
+            # Window can be expanded without whitespace by moving image
+            interval[1] = image_length
+            interval[0] = interval[1] - newsize
+        else:
+            # Set maximum along this length
+            interval[0] = 0
+            interval[1] = image_length
 
     def resize_action(self, newsize):
         "Respond as appropriate to a resize event."
-        orig = (self.xint[1], self.yint[1])
-        self.xint[1] = min(self.xint[0]+newsize[0],
-                           int(self.isize[0]*self.zoom))
-        self.yint[1] = min(self.yint[0]+newsize[1],
-                           int(self.isize[1]*self.zoom))
-        if (self.xint[1], self.yint[1]) != orig:
+        if (newsize[0] > self.zoom * self.isize[0] or
+            newsize[1] > self.zoom * self.isize[1]):
+            # Resize message too big; reset and deal with a smaller one
+            # Don't pragate this message; you'll get another one RSN
+            print newsize, self.xint, self.yint
+            self.canvas["width"] = min(newsize[0], self.xint[1])
+            self.canvas["height"] = min(newsize[1], self.yint[1])
+        orig = (self.xint[0], self.xint[1], self.yint[0], self.yint[1])
+        self.resize_view_axis(self.xint, newsize[0], self.isize[0])
+        self.resize_view_axis(self.yint, newsize[1], self.isize[1])
+        self.display_size = (self.xint[1] - self.xint[0], self.yint[1] - self.yint[0])
+        if (self.xint[0], self.xint[1], self.yint[0], self.yint[1]) != orig:
             self.refresh()
 
     def drag_action(self, diff):
@@ -175,13 +316,13 @@ class ImageWidget(Frame):
     def xview_action(self, subaction, *args):
         "Handle an xscroll request from a scrollbar."
         orig = [i for i in self.xint]
-        self.starview_action(self.xint, self.isize[0], subaction, *args)
+        self.starview_action(self.xint, int(self.isize[0] * self.zoom), subaction, *args)
         if orig != self.xint: self.refresh()
 
     def yview_action(self, subaction, *args):
         "Handle an yscroll request from a scrollbar."
         orig = [i for i in self.yint]
-        self.starview_action(self.yint, self.isize[1], subaction, *args)
+        self.starview_action(self.yint, int(self.isize[1] * self.zoom), subaction, *args)
         if orig != self.yint: self.refresh()
 
     # Helper methods for {drag,xview,yview}_action
@@ -265,50 +406,36 @@ class ImageWidget(Frame):
             self.evv_dragStart = None
 
     def ev_MouseWheel(self, event):
-        self.scrollWheel_action(event.delta)
+        self.scrollWheel_action(event.delta, (event.x,event.y))
 
     def ev_Configure(self, event):
+        print "ev_Configure: ", event.serial, (event.width, event.height)
         self.resize_action((event.width, event.height))
 
-    def refresh(self):
-        """Bring the image in the frame and the scroll bars in line with the
-        current values.  Note that this is not done for scrolling; that's
-        taken care of directly by the scrollbar/canvas interactions.
-        xint & yint are updated automatically for scrolling."""
-
-        # Delete old image (if needed) and create new one
-        if self.canvas_image_id:
-            self.canvas.delete(self.canvas_image_id)
-        if debug > 5:
-            print "refresh: New image (x", self.zoom, ") ", self.xint, self.yint
-        self.image = self.generator_func(self.zoom, self.xint, self.yint)
-
-        self.canvas_image_id = self.canvas.create_image(0, 0, anchor=N+W,
-                                                        image=self.image)
-
-        # Figure out where scroll bars should be and put them there.
-        scroll_settings = (
-            (mapped_number(self.xint[0],
-                    (0, self.isize[0] * self.zoom -1),
-                    (0, 1)),
-             mapped_number(self.xint[1] -1,
-                    (0, self.isize[0] * self.zoom -1),
-                    (0, 1))),
-            (mapped_number(self.yint[0],
-                    (0, self.isize[1] * self.zoom -1),
-                    (0, 1)),
-             mapped_number(self.yint[1] -1,
-                    (0, self.isize[1] * self.zoom -1),
-                    (0, 1))))
-        if debug > 5:
-            print scroll_settings
-        self.hscroll.set(*scroll_settings[0])
-        self.vscroll.set(*scroll_settings[1])
-
-
-
-
 ## Room for optimization here; don't need to resize the whole image
+class GfuncImageWrapper:
+    def __init__(self, baseimage):
+        self.imageStore = {}
+        self.imageStore[1.0] = baseimage
+
+    # XXX: Worth rounding zoom?
+    def __call__(self, zoom, xint, yint):
+        if debug > 5:
+            print "GFunc for image: ", zoom, xint, yint
+        if zoom not in self.imageStore:
+            bi = self.imageStore[1.0]
+            bbox = bi.getbbox()
+            isize = (bbox[2] - bbox[0], bbox[3] - bbox[1])
+            ssize = (int(isize[0] * zoom), int(isize[1] * zoom))
+
+            self.imageStore[zoom] = bi.resize(ssize, Image.BILINEAR)
+
+        ri = self.imageStore[zoom]
+        ci = ri.crop((xint[0],yint[0],xint[1],yint[1]))
+        ki = ImageTk.PhotoImage(ci)
+        return ki
+        
+
 def gfunc_for_image(image, zoom, xint, yint):
     bbox = image.getbbox()
     isize = (bbox[2] - bbox[0], bbox[3] - bbox[1])
@@ -326,7 +453,7 @@ def IWFromFile(parent, file, **kwargs):
     baseimage = Image.open(file)
     (ulx, uly, lrx, lry) = baseimage.getbbox()
     return ImageWidget(parent,
-                       lambda z,xint,yint,i=baseimage: gfunc_for_image(i, z, xint, yint),
+                       GfuncImageWrapper(baseimage),
                        (lrx - ulx, lry - uly),
                        **kwargs)
 
@@ -334,10 +461,9 @@ def IWFromImage(parent, img, **kwargs):
     "Return an imageWidget object based on a PIL image passed in."
     (ulx, uly, lrx, lry) = img.getbbox()
     return ImageWidget(parent,
-                       lambda z,xint,yint,i=img: gfunc_for_image(i, z, xint, yint),
+                       GfuncImageWrapper(img)
                        (lrx - ulx, lry - uly),
                        **kwargs)
-
 
 if __name__ == "__main__":
     root = Tk()
@@ -346,8 +472,9 @@ if __name__ == "__main__":
     # root.bind("<Configure>", lambda e, t="root": dbg_display_tag_and_size(t, e))
     iw = IWFromFile(root, "iw_test.tiff", starting_ul = (100,100),
                     starting_size = (200, 200), starting_zoom = 1.0,
-                    mouse_click_function = dbg_print_coords)
-    iw.grid(row=0,column=0,sticky=N+S+E+W)
+                    mouse_click_function = dbg_print_coords,
+                    maxsize_callback = lambda w,h,r=root: r.maxsize(w,h))
+    iw.grid(row=0,column=0, sticky=N+S+E+W)
     root.rowconfigure(0, weight=1)
     root.columnconfigure(0, weight=1)
     iw.mainloop()
